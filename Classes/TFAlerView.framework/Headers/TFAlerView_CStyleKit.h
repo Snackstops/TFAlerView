719b4867a95c94dda92e335eb5b34222514a4e7e
apple <snackstops@MacBook-Pro.local>
2020-04-07 14:37:02 +0800

    提交内容

---
+++ Classes/TFAlerView.framework/Headers/TFAlerView_CStyleKit.h
@@ -0,0 +1,104 @@
+//
+//  TFAlerView_CStyleKit.h
+//  PhotoBrowseDemo
+//
+//  Created by snackstops on 2020/4/1.
+//  Copyright © 2020 TFAlerView. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "TFAlerView_CButton.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TFAlerView_CStyleKit : NSObject
+
+// Images
+/** TODO
+ *
+ * TODO
+ */
++ (UIImage *)imageOfCheckmark;
+
+/** TODO
+ *
+ * TODO
+ */
++ (UIImage *)imageOfCross;
+
+/** TODO
+ *
+ * TODO
+ */
++ (UIImage *)imageOfNotice;
+
+/** TODO
+ *
+ * TODO
+ */
++ (UIImage *)imageOfWarning;
+
+/** TODO
+ *
+ * TODO
+ */
++ (UIImage *)imageOfInfo;
+
+/** TODO
+ *
+ * TODO
+ */
++ (UIImage *)imageOfEdit;
+
+/** TODO
+ *
+ * TODO
+ */
++ (UIImage *)imageOfQuestion;
+
+/** TODO
+ *
+ * TODO
+ */
++ (void)drawCheckmark;
+
+/** TODO
+ *
+ * TODO
+ */
++ (void)drawCross;
+
+/** TODO
+ *
+ * TODO
+ */
++ (void)drawNotice;
+
+/** TODO
+ *
+ * TODO
+ */
++ (void)drawWarning;
+
+/** TODO
+ *
+ * TODO
+ */
++ (void)drawInfo;
+
+/** TODO
+ *
+ * TODO
+ */
++ (void)drawEdit;
+
+/** TODO
+ *
+ * TODO
+ */
++ (void)drawQuestion;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/TFAlerView.framework/Headers/TFAlerView_CTextView.h
@@ -0,0 +1,17 @@
+//
+//  TFAlerView_CTextView.h
+//  PhotoBrowseDemo
+//
+//  Created by snackstops on 2020/4/1.
+//  Copyright © 2020 TFAlerView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TFAlerView_CTextView : UITextField
+
+@end
+
+NS_ASSUME_NONNULL_END
